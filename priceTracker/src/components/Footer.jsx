34cffb06dd4c1d6864f2b0ed0f4baade9f6eab61
aadarshCode <adarshpathak54@gmail.com>
2025-01-04 14:17:13 +0530

    footer alignment fixed

--- priceTracker/src/components/Footer.jsx
+++ priceTracker/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-gray-800 text-white py-4 text-center">
+    <footer className=" w-full bg-gray-800 text-white py-4 text-center">
       <p className="text-sm md:text-base">
         Made by <span className="font-bold">Aadarsh Pathak</span> ©{" "}
         {currentYear}
